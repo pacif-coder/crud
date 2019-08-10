aa390ef94a92e0c5f53211ab1ebf7b35a8edc4a0
Pacif.coder <pacif.coder@gmail.com>
2019-08-10 11:30:50 +0700

    if the GridView is empty - do not show buttons that work with selected rows

--- modules/crud/grid/toolbar/SendFormButton.php
+++ modules/crud/grid/toolbar/SendFormButton.php
@@ -7,29 +7,37 @@ use yii\helpers\Url;
 use yii\grid\CheckboxColumn;
 
 use app\modules\crud\grid\toolbar\Button;
+use app\modules\crud\grid\toolbar\NeedCheckboxColumnInterface;
 
 /**
  * Description of Delete
  *
  */
-class SendFormButton extends Button {
-    public $action = [];
+class SendFormButton extends Button implements NeedCheckboxColumnInterface {
+    public $action;
 
     public $options = ['data-role' => 'grid-button-send'];
 
     public function getAttrs() {
         $attrs = parent::getAttrs();
         if ($this->action) {
-            $attrs['data-url'] = Url::to($this->action);
+            $action = is_string($this->action)? [$this->action] : $this->action;
+            $attrs['data-url'] = Url::to($action);
         }
 
-        $this->addCsrf($attrs);
         foreach ($this->grid->columns as $column) {
             if ($column instanceof CheckboxColumn) {
                 $attrs['data-checkbox-name'] = $column->name;
+                break;
             }
         }
 
         return $attrs;
+    }
+
+    public function html() {
+        if ($this->grid->dataProvider->getTotalCount()) {
+            return parent::html();
+        }
     }
 }
