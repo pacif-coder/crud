b0dbff7266fb1d956ed4cb85fc5814b8b244e085
Pacif.coder <pacif.coder@gmail.com>
2019-08-25 02:34:09 +0700

    collect action - not enened

--- modules/crud/grid/column/CheckboxColumn.php
+++ modules/crud/grid/column/CheckboxColumn.php
@@ -1,9 +1,34 @@
 <?php
 namespace app\modules\crud\grid\column;
 
+use yii\helpers\Html;
+use yii\helpers\Json;
+
 /**
  *
  */
 class CheckboxColumn extends \yii\grid\CheckboxColumn {
     public $headerOptions = ['class' => 'checkbox-column'];
+
+    public $checkedActions = [];
+
+    public $checkPermission = null;
+
+    protected function renderDataCellContent($model, $key, $index) {
+        if ($this->checkboxOptions instanceof Closure) {
+            $options = call_user_func($this->checkboxOptions, $model, $key, $index, $this);
+        } else {
+            $options = $this->checkboxOptions;
+        }
+
+        if (!isset($options['value'])) {
+            $options['value'] = is_array($key) ? Json::encode($key) : $key;
+        }
+
+        if ($this->cssClass !== null) {
+            Html::addCssClass($options, $this->cssClass);
+        }
+
+        return Html::checkbox($this->name, !empty($options['checked']), $options);
+    }
 }
