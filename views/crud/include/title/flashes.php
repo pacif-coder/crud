8a62bdbffa9fcc0c212a6ba742a08564833f4717
pacif <pacif@FreeLand>
2022-01-28 15:10:38 +0700

    More icon by alert tyoe

--- views/crud/include/title/flashes.php
+++ views/crud/include/title/flashes.php
@@ -2,17 +2,31 @@
 use yii\bootstrap\Html;
 
 foreach (Yii::$app->session->getAllFlashes() as $type => $message) {
+    $icon = '';
     switch ($type) {
         case 'success':
             $icon = 'ok';
             break;
 
-        default:
+        case 'warning':
+            $icon = 'question-sign';
+            break;
+
+        case 'info':
+            $icon = 'info-sign';
+            break;
+
+        case 'danger':
             $icon = 'remove-sign';
+            break;
+    }
+
+    if ($icon) {
+        $icon = Html::icon($icon);
     }
     ?>
         <div class="alert alert-<?=$type?>">
-            <?=Html::icon($icon)?>
+            <?=$icon?>
             <?=nl2br(is_array($message)? implode("\n", $message) : $message)?>
         </div>
     <?php
