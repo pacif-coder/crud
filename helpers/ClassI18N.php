25de32287b141fdd3395cd5a48e51bae2e59f549
pacif <pacif@FreeLand>
2022-02-15 11:27:15 +0700

    code format

--- helpers/ClassI18N.php
+++ helpers/ClassI18N.php
@@ -7,9 +7,10 @@ use yii\helpers\Inflector;
  * Description of Class
  *
  */
-class ClassI18N {
-
-    public static function class2messagesPath($class) {
+class ClassI18N
+{
+    public static function class2messagesPath($class)
+    {
         $pos = strrpos($class, '\\');
         if (false != $pos) {
             $class = substr($class, 0, $pos) . '\\' . Inflector::camel2id(lcfirst(substr($class, $pos + 1)));
