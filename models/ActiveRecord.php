85613a2fb50bb8d222ef24ebd73ce0573118b037
pacif <pacif@FreeLand>
2022-02-15 11:30:33 +0700

    Correct variable initialization

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -11,41 +11,41 @@ use app\modules\crud\helpers\ClassI18N;
 class ActiveRecord extends \yii\db\ActiveRecord
 {
     protected static $fb_fields;
-    protected static $fb_removeFields;
-    protected static $fb_addFieldsAfter;
+    protected static $fb_removeFields = [];
+    protected static $fb_addFieldsAfter = [];
 
     protected static $fb_fieldTypes;
     protected static $fb_type2fields;
     protected static $fb_fieldOptions;
-    protected static $fb_fieldAddClass;
+    protected static $fb_fieldAddClass = [];
 
-    protected static $fb_readyOnlyFields;
+    protected static $fb_readyOnlyFields = [];
 
-    protected static $fb_fieldHint;
+    protected static $fb_fieldHint = [];
 
     protected static $fb_fieldset2fields;
-    protected static $fb_fieldsetLegends;
-    protected static $fb_fieldsetAttrs;
-    protected static $fb_fieldsBeforeFieldsetLegend;
-    protected static $fb_fieldsAfterFieldsetLegend;
+    protected static $fb_fieldsetLegends = [];
+    protected static $fb_fieldsetAttrs = [];
+    protected static $fb_fieldsBeforeFieldsetLegend = [];
+    protected static $fb_fieldsAfterFieldsetLegend = [];
 
-    protected static $fb_fieldType2fieldMethod;
-    protected static $fb_fieldType2widget;
-    protected static $fb_fieldType2widgetOptions;
+    protected static $fb_fieldType2fieldMethod = [];
+    protected static $fb_fieldType2widget = [];
+    protected static $fb_fieldType2widgetOptions = [];
 
     protected static $fb_enumFields;
     protected static $fb_enumOptions;
-    protected static $fb_translationEnumOptions;
-    protected static $fb_addEmptyEnumOption;
-    protected static $fb_emptyEnumOptionLabel;
+    protected static $fb_translationEnumOptions = [];
+    protected static $fb_addEmptyEnumOption = true;
+    protected static $fb_emptyEnumOptionLabel = '---';
 
     protected static $fb_formExtraControls = ['save', 'cancel'];
     protected static $fb_gridExtraControls = ['create'];
-    protected static $fb_addExtraControls;
-    protected static $fb_removeExtraControls;
-    protected static $fb_extraControlOptions;
+    protected static $fb_addExtraControls = [];
+    protected static $fb_removeExtraControls = [];
+    protected static $fb_extraControlOptions = [];
 
-    protected static $fb_extraProcessedFields;
+    protected static $fb_extraProcessedFields = [];
 
     public function attributeLabels()
     {
