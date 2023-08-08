05799dfda17e47980fcbd2ce7600d84522025e8a
pacif-coder <pacif.coder@gmail.com>
2023-08-08 22:52:25 +0600

    move to yii2 extenstion

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -1,8 +1,9 @@
 <?php
-namespace app\modules\crud\models;
+namespace Crud\models;
 
 use Yii;
-use app\modules\crud\helpers\ClassI18N;
+use Crud\helpers\ClassI18N;
+use Crud\helpers\Lang;
 
 /**
  *
@@ -10,50 +11,12 @@ use app\modules\crud\helpers\ClassI18N;
  */
 class ActiveRecord extends \yii\db\ActiveRecord
 {
-    protected static $fb_fields;
-    protected static $fb_removeFields = [];
-    protected static $fb_addFieldsAfter = [];
-
-    protected static $fb_fieldTypes;
-    protected static $fb_type2fields;
-    protected static $fb_fieldOptions;
-    protected static $fb_fieldAddClass = [];
-
-    protected static $fb_readyOnlyFields = [];
-
-    protected static $fb_fieldHint = [];
-
-    protected static $fb_fieldset2fields;
-    protected static $fb_fieldsetLegends = [];
-    protected static $fb_fieldsetAttrs = [];
-    protected static $fb_fieldsBeforeFieldsetLegend = [];
-    protected static $fb_fieldsAfterFieldsetLegend = [];
-
-    protected static $fb_fieldType2fieldMethod = [];
-    protected static $fb_fieldType2widget = [];
-    protected static $fb_fieldType2widgetOptions = [];
-
-    protected static $fb_enumFields;
-    protected static $fb_enumOptions;
-    protected static $fb_translationEnumOptions = [];
-    protected static $fb_addEmptyEnumOption = true;
-    protected static $fb_emptyEnumOptionLabel = '---';
-
-    protected static $fb_formExtraControls = ['save', 'cancel'];
-    protected static $fb_gridExtraControls = ['create'];
-    protected static $fb_addExtraControls = [];
-    protected static $fb_removeExtraControls = [];
-    protected static $fb_extraControlOptions = [];
-
-    protected static $fb_extraProcessedFields = [];
-
     public function attributeLabels()
     {
-        $file = ClassI18N::class2messagesPath(static::class);
-
         $list = [];
+        $category = ClassI18N::class2messagesPath(static::class);
         foreach ($this->attributes() as $attribute) {
-            $list[$attribute] = Yii::t($file, $attribute);
+            $list[$attribute] = Lang::t($category, $attribute);
         }
 
         return $list;
--- models/ModelWithNameAttrInterface.php
+++ models/ModelWithNameAttrInterface.php
@@ -1,5 +1,5 @@
 <?php
-namespace app\modules\crud\models;
+namespace Crud\models;
 
 /**
  * 
