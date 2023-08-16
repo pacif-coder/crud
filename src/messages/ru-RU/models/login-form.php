4aab39059b879cc8d19bd6ec3c37220b35368426
pacif-coder <pacif.coder@gmail.com>
2023-08-16 14:50:25 +0600

    form for login action

---
+++ src/messages/ru-RU/models/login-form.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'username' => 'Логин',
+    'password' => 'Пароль',
+
+    'loginSubmit' => 'Войти',
+
+    'Incorrect username or password' => 'Неверный логин или пароль',
+
+    'Username cannot be blank' => 'Необходимо заполнить логин',
+    'Password cannot be blank' => 'Необходимо заполнить пароль',
+];
