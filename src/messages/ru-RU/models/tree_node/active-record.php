49a95288cca288bd2d18d91edffcf955509bfab4
pacif-coder <bootbash@ya.ru>
2024-05-20 09:37:08 +0600

    add default message for activ record class

---
+++ src/messages/ru-RU/models/tree_node/active-record.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Top list items' => 'Список разделов',
+    'List items' => 'Список в разделе "{parentModelName}"',
+    'Update item "{nameAttribute}"' => 'Изменить "{nameAttribute}"',
+    'Create item' => 'Создать',
+
+    'name' => 'Имя',
+    'path' => 'Имя в ссылке',
+];
---
+++ src/messages/ru-RU/models/tree_node/folder.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Top list items' => 'Список разделов',
+    'List items' => 'Список в разделе "{parentModelName}"',
+    'Update item "{nameAttribute}"' => 'Изменить раздел "{nameAttribute}"',
+    'Create item' => 'Создать раздел',
+];
