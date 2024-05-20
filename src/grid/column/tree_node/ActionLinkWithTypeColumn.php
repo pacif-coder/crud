2c4d9fba59161fc41ca80037c598b13420926b8c
pacif-coder <bootbash@ya.ru>
2024-05-20 09:35:46 +0600

    ActionLinkWithTypeColumn - use in tree mode

---
+++ src/grid/column/tree_node/ActionLinkWithTypeColumn.php
@@ -0,0 +1,48 @@
+<?php
+namespace Crud\grid\column\tree_node;
+
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\db\ActiveQuery;
+
+use Crud\helpers\ChildeClass;
+use Crud\models\tree_node\Type;
+
+/**
+ *
+ */
+class ActionLinkWithTypeColumn extends \Crud\grid\column\ActionLinkColumn
+{
+    protected $indexActions = [
+        'index',
+    ];
+
+    protected function getUrlParams($action, $model, $key, $index)
+    {
+        $params = parent::getUrlParams($action, $model, $key, $index);
+
+        if (!($this->grid->dataProvider instanceof ActiveDataProvider)) {
+            return $params;
+        }
+
+        if (!($this->grid->dataProvider->query instanceof ActiveQuery)) {
+            return $params;
+        }
+
+        /* @var $this->grid->dataProvider->query ActiveQuery */
+        $class = $this->grid->dataProvider->query->modelClass;
+
+        if (in_array($action, $this->indexActions)) {
+            $modelData = $model;
+            foreach ($this->grid->renamedLink2ModelAttr as $renameAttr => $attr) {
+                $modelData[$renameAttr] = $modelData[$attr];
+                unset($modelData[$attr]);
+            }
+
+            $class = ChildeClass::getChildeClass($class, $modelData);
+        }
+        $params['type'] = Type::getTypeByClass($class);
+
+        return $params;
+    }
+}
