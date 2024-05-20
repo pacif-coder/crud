d28ec7bf807f3403223b0f1ac4fa2acf0edcf49f
pacif-coder <bootbash@ya.ru>
2024-05-20 09:30:47 +0600

    second action link on icon

--- src/grid/column/ActionLinkColumn.php
+++ src/grid/column/ActionLinkColumn.php
@@ -1,7 +1,7 @@
 <?php
 namespace Crud\grid\column;
 
-use yii\helpers\Html;
+use Crud\helpers\Html;
 
 /**
  * [[GridView]] widget that displays buttons for viewing and manipulating the items.
@@ -36,33 +36,86 @@ class ActionLinkColumn extends \yii\grid\DataColumn
      */
     public $addHash = true;
 
+    /**
+     * @var string
+     */
+    public $secondAction;
+
+    /**
+     * Add 'back-url' param in url
+     * @var boolean
+     */
+    public $secondActionBackUrl;
+
+    /**
+     *
+     * @var string
+     */
+    public $secondActionIcon = 'pencil-square';
+
+    /**
+     *
+     * @var string
+     */
+    public $secondActionClass = 'text-success';
+
+    protected $secondActionContAttrs = [
+        'class' => 'action-link-column-with-second-link',
+    ];
+
     protected function renderDataCellContent($model, $key, $index)
     {
+        $array = $this->asArray($model, $key, $index);
+
+        // only text - no link
+        if (isset($array['text']) && count($array) == 1) {
+            return $array;
+        }
+
+        // create main link
+        $attrs = $this->addLinkClass? ['class' => $this->addLinkClass] : [];
+        $link = Html::a($array['text'], $array['link'], $attrs);
+        if (!isset($array['secondLink'])) {
+            return $this->surround($link, $index);
+        }
+
+        // create second link
+        $icon = $this->getSecondActionIcon($model, $key, $index);
+        $secondLink = Html::a($icon, $array['secondLink']);
+
+        // create block with main and second link
+        $str = Html::tag('div', $link . $secondLink, $this->secondActionContAttrs);
+        return $this->surround($str, $index);
+    }
+
+    public function asArray($model, $key, $index)
+    {
+        $asArray = [];
         if (null !== $this->linkText) {
             $text = $this->linkText;
         } else {
             $text = parent::renderDataCellContent($model, $key, $index);
         }
+        $asArray['text'] = $text;
 
         if (!$this->checkPermission($model, $key, $index)) {
-            return $text;
-        }
-
-        $str = '';
-        if ($this->addHash) {
-            $str .= Html::a('', null, ['name' => $index]);
+            return $asArray;
         }
 
         $action = $this->getAction($model, $key, $index);
-        $url = $this->createUrl($action, $model, $key, $index);
+        $asArray['link'] = $this->createUrl($action, $model, $key, $index, $this->backUrl);
 
-        $attrs = [];
-        if ($this->addLinkClass) {
-            Html::addCssClass($attrs, $this->addLinkClass);
+        if (!$this->secondAction) {
+            return $asArray;
         }
 
-        $str .= Html::a($text, $url, $attrs);
-        return $this->truncateContent($str);
+        $secondAction = $this->getSecondAction($model, $key, $index);
+        if ($action == $secondAction) {
+            return $asArray;
+        }
+
+        $asArray['secondLink'] = $this->createUrl($secondAction, $model, $key, $index, $this->secondActionBackUrl);
+        return $asArray;
     }
 
     protected function getAction($model, $key, $index)
@@ -72,5 +125,30 @@ class ActionLinkColumn extends \yii\grid\DataColumn
         }
 
         return $this->action;
+    }
+
+    protected function getSecondAction($model, $key, $index)
+    {
+        if (is_callable($this->secondAction)) {
+            return call_user_func($this->secondAction, $model, $key, $index, $this);
+        }
+
+        return $this->secondAction;
+    }
+
+    protected function getSecondActionIcon($model, $key, $index)
+    {
+        return Html::icon($this->secondActionIcon, $this->secondActionClass);
+    }
+
+    protected function surround($str, $index)
+    {
+        $str = $this->truncateContent($str);
+
+        if ($this->addHash) {
+            $str = Html::a('', null, ['name' => $index]) . $str;
+        }
+
+        return $str;
     }
 }
