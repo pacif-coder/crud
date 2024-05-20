af071231e7fdf7beb1337b06c1388873e1118587
pacif-coder <bootbash@ya.ru>
2024-05-20 09:48:43 +0600

    rename autocomplite

--- src/widgets/AutoComplete.php
+++ src/widgets/AutoComplete.php
@@ -6,11 +6,13 @@ use yii\helpers\Url;
 
 use Crud\widgets\assets\JuiWidgetAsset;
 
-class Autocomplete extends \yii\widgets\InputWidget
+class AutoComplete extends \yii\widgets\InputWidget
 {
     public $sourceRoute;
 
     public $source = [];
+
+    protected static $type = 'autocomplete';
 
     public function run()
     {
@@ -18,15 +20,19 @@ class Autocomplete extends \yii\widgets\InputWidget
 
         $options = $this->options;
         $options['id'] = $this->getId();
-        $options['data-widget'] = 'autocomplete';
-
-        if (null !== $this->sourceRoute) {
-            $options['data-source'] = Url::toRoute($this->sourceRoute);
-        } else {
-            $options['data-source'] = $this->source;
-        }
+        $options['data-widget'] = static::$type;
+        $options['data-source'] = $this->getSource();
 
         return Html::activeTextInput($this->model, $this->attribute, $options);
+    }
+
+    public function getSource()
+    {
+        if (null !== $this->sourceRoute) {
+            return Url::toRoute($this->sourceRoute);
+        }
+
+        return $this->source;
     }
 
     public function registerClientScript()
