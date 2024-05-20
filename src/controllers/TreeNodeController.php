2ad70a76314e5ab4477bf97e5a29eeb35bb4bb31
pacif-coder <bootbash@ya.ru>
2024-05-20 09:18:08 +0600

    first dirty verion for tree node controller

--- src/controllers/TreeNodeController.php
+++ src/controllers/TreeNodeController.php
@@ -2,100 +2,46 @@
 namespace Crud\controllers;
 
 use Yii;
-use yii\web\BadRequestHttpException;
 
-use Crud\data\TreeNodeDelete;
-use Crud\helpers\ClassI18N;
-use Crud\models\ModelWithParentInterface;
-use Crud\models\tree_node\Node;
+use Crud\grid\column\tree_node\ActionLinkWithTypeColumn;
+use Crud\helpers\ModelName;
+use Crud\models\tree_node\ActiveRecord;
+use Crud\models\tree_node\Folder;
 use Crud\models\tree_node\Type;
-use Crud\models\tree_node\WithPathInterface;
 use Crud\widgets\TreeMenu;
+
+use Exception;
 
 /**
  *
  */
 class TreeNodeController extends CrudController
 {
-    public $hasTreeMenu = true;
-
-    protected $createTypeCode;
-
-    protected $treeMenuAttrs = [];
+    protected $rootClass = Folder::class;
 
     public function init()
     {
-        $this->modelClass = Node::getClass();
+        $this->globalUseClass[] = TreeMenu::class;
 
         parent::init();
     }
 
-    protected function _actionEdit($isCreate)
+    public function fillModelClass()
     {
-        $modelClass = null;
+        $type = Yii::$app->request->get('type');
+        $id = Yii::$app->request->get('id');
 
-        if ($isCreate) {
-            $typeID = $this->request->get('type');
-            if (!$typeID) {
-                throw new BadRequestHttpException(
-                    Yii::t('yii', 'Missing required parameters: {params}', ['params' => 'type'])
-                );
-            }
+        if (!$id && !$type) {
+            $this->modelClass = $this->rootClass;
 
-            $modelClass = Type::getClassByType($typeID);
-        } else {
-            $node = $this->_findModel(Node::getClass(), $this->getModelID());
-            $modelClass = Type::getClassByType($node->type_id);
+            return;
         }
 
-        if ($modelClass) {
-            $this->modelClass = $modelClass;
-            $this->messageCategory = ClassI18N::class2messagesPath($modelClass);
+        if ($id && !$type) {
+            throw new Exception('Type mast be definded');
         }
 
-        return parent::_actionEdit($isCreate);
-    }
-
-    public function actionDelete()
-    {
-        if (!is_a($this->modelClass, Node::getClass(), true)) {
-            return parent::actionDelete();
-        }
-
-        $selection = $this->request->post('selection', []);
-        $delete = new TreeNodeDelete();
-        $delete->deleteBranchs($selection);
-
-        if ($delete->errors) {
-            $this->addFlashMessage('danger', implode("\n", $delete->errors));
-        } else {
-            $this->addFlashMessage('info', implode("\n", $delete->messages));
-        }
-
-        return $this->goBack();
-    }
-
-    protected function createEditBreadcrumbs($isCreate, $model)
-    {
-        if (!is_a($model, ModelWithParentInterface::class)) {
-            return parent::createEditBreadcrumbs($isCreate, $model);
-        }
-
-        $node = Node::factory();
-        $node->id = $model->id;
-        $node->parent_id = $model->parent_id;
-
-        return parent::createEditBreadcrumbs($isCreate, $node);
-    }
-
-    protected function createModel()
-    {
-        $model = parent::createModel();
-        if (is_a($model, Node::class)) {
-            $model->type_id = $this->request->get('type');
-        }
-
-        return $model;
+        $this->modelClass = Type::getClassByType($type);
     }
 
     protected function beforeGridBuild(\yii\base\Event $event)
@@ -103,60 +49,29 @@ class TreeNodeController extends CrudController
         /* @var $gridBuilder \Crud\builder\GridBuilder */
         $gridBuilder = $event->sender;
 
-        $gridBuilder->columnOptions['name']['action'] = [self::class, 'getNodeAction'];
+        $gridBuilder->editColumnClass = ActionLinkWithTypeColumn::class;
 
-        $typeCode = $this->getCreateTypeCode();
-        if ($typeCode) {
-            $gridBuilder->extraControlOptions['create']['params'] = [
-                'type' => Type::getTypeByCode($typeCode),
-            ];
-        } else {
-            $gridBuilder->gridExtraControls = [];
+        $nameAttr = ModelName::getNameAttr($this->modelClass);
+        $type = Type::getTypeByClass($this->modelClass);
+        $action = Type::isFolderByType($type)? 'index' : 'update';
+
+        if (!isset($gridBuilder->columnOptions[$nameAttr]['action'])) {
+            $gridBuilder->columnOptions[$nameAttr]['action'] = $action;
+        }
+
+        if ('index' == $action) {
+            $gridBuilder->columnOptions[$nameAttr]['secondAction'] = 'update';
+
+            $gridBuilder->columnOptions[$nameAttr]['backUrl'] = false;
+
+            $gridBuilder->columnOptions[$nameAttr]['secondActionBackUrl'] = true;
         }
     }
 
-    protected function afterGridBuild(\yii\base\Event $event)
+    protected function createBreadcrumbs()
     {
-        /* @var $gridBuilder \Crud\builder\GridBuilder */
-        $gridBuilder = $event->sender;
+        $this->breadcrumbs += ['withType' => true];
 
-        if (null === $gridBuilder->parentModelID) {
-            return;
-        }
-
-        $node = $this->findModel();
-        $class = Type::getClassByType($node->type_id);
-        if ($class && !is_a($class, WithPathInterface::class, true)) {
-            unset($gridBuilder->columns['path']);
-        }
-    }
-
-    protected function getCreateTypeCode()
-    {
-    }
-
-    public function getTreeMenu()
-    {
-        $treeMenuAttrs = $this->treeMenuAttrs;
-        $treeMenuAttrs['currentId'] = $this->getModelID();
-
-        $menu = new TreeMenu($treeMenuAttrs);
-        return $menu->html();
-    }
-
-    public static function getNodeAction($node, $key, $index, $colunn)
-    {
-        if (!isset($colunn->grid->renamedLink2ModelAttr['type_id'])) {
-            return 'update';
-        }
-
-        $typeRenameAttr = $colunn->grid->renamedLink2ModelAttr['type_id'];
-        if (is_object($node)) {
-            $type = $node->{$typeRenameAttr};
-        } else {
-            $type = $node[$typeRenameAttr];
-        }
-
-        return Type::isFolderByType($type)? 'index' : 'update';
+        parent::createBreadcrumbs();
     }
 }
