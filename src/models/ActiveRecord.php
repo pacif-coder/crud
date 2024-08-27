6cb271a8a089b403032880825e996d241fa854ac
pacif-coder <pacif.coder@gmail.com>
2024-08-27 11:36:45 +0500

    cashed attributeLabels

--- src/models/ActiveRecord.php
+++ src/models/ActiveRecord.php
@@ -1,6 +1,8 @@
 <?php
 namespace Crud\models;
 
+use Yii;
+
 use Crud\helpers\Lang;
 
 /**
@@ -9,23 +11,35 @@ use Crud\helpers\Lang;
  */
 class ActiveRecord extends \yii\db\ActiveRecord
 {
+    protected static $class2labels = [];
+
     protected static $extendAttrsLabel = [];
+
+    /**
+     *
+     */
+    // protected static $skipMessageCategory = true;
 
     public function attributeLabels()
     {
+        if (isset(self::$class2labels[static::class])) {
+            return self::$class2labels[static::class];
+        }
+
         $list = [];
         $category = static::getMessageCategory();
-        $attrs = array_merge($this->attributes(), static::$extendAttrsLabel);
+        $extendAttrsLabel = Lang::getExtendAttrsLabel(static::class);
+
+        $attrs = array_merge($this->attributes(), $extendAttrsLabel);
         foreach ($attrs as $attr) {
             $list[$attr] = Lang::t($category, $attr);
         }
 
-        return $list;
+        return self::$class2labels[static::class] = $list;
     }
 
     public static function getMessageCategory()
     {
-        $classes = Lang::regClassPref(static::class, 'models');
-        return Lang::classes2messagesCategory($classes);
+        return Lang::getParentCategorysByRel(static::class, 'models');
     }
 }
